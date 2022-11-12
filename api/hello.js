c109073527c8d17b90628052b8284d951e8130a2
_lmmmmmm <lmmmmmm12138@gmail.com>
2022-11-12 15:22:11 +0800

    fix: remove type in js

--- api/hello.js
+++ api/hello.js
@@ -1,6 +1,4 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default (req: VercelRequest, res: VercelResponse) => {
-	const { name = 'lmmmmmm' } = req.query;
-	return res.send(`Hello ${name}`);
+export default (request, response) => {
+	const { name = 'lmmmmmm' } = request.query;
+	return response.send(`Hello ${name}`);
 }
