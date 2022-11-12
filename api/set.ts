67a36a142238c9b8bd7e4df317d799eb5bdf468f
_lmmmmmm <lmmmmmm12138@gmail.com>
2022-11-12 20:29:17 +0800

    fix: await set webhook

--- api/set.ts
+++ api/set.ts
@@ -6,7 +6,7 @@ export default async (request: VercelRequest, response: VercelResponse) => {
   const { wb } = request.query;
 
   if (wb && !Array.isArray(wb)) {
-    bot.telegram.setWebhook(wb);
+    await bot.telegram.setWebhook(wb);
     response.send(`Set Webhook Success! ${wb}`);
   }
 
