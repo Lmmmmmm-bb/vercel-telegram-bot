76abbdf9d073a8e31afe9a3c12802cd849ac63be
_lmmmmmm <lmmmmmm12138@gmail.com>
2022-11-12 16:01:21 +0800

    chore: bot instance handler inside

--- api/tele.js
+++ api/tele.js
@@ -2,18 +2,19 @@ import { Telegraf } from "telegraf";
 
 const BOT_TOKEN = process.env.BOT_TOKEN;
 
-const bot = new Telegraf(BOT_TOKEN);
-
-bot.on('message', async (ctx) => {
-	ctx.reply(ctx.message.text);
-})
-
 export default async (request, response) => {
 	const { webhook } = request.query;
 
 	if (webhook) {
 		bot.telegram.setWebhook(webhook);
 		response.send(`webhook set ${webhook}`);
+	} else {
+		const bot = new Telegraf(BOT_TOKEN);
+
+		bot.on('message', async (ctx) => {
+			ctx.reply(ctx.message.text);
+		})
+
 	}
 
 	response.send(`telegram bot say Hi.`);
