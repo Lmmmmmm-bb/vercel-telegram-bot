d3e4599a019f0b25143ddfd6f66abbdd20193d8c
_lmmmmmm <lmmmmmm12138@gmail.com>
2022-11-12 16:43:02 +0800

    fix: bot update

--- api/tele.js
+++ api/tele.js
@@ -2,6 +2,12 @@ import { Telegraf } from "telegraf";
 
 const BOT_TOKEN = process.env.BOT_TOKEN;
 
+const bot = new Telegraf(BOT_TOKEN);
+
+bot.on('message', async (ctx) => {
+	ctx.reply(ctx.message.text);
+})
+
 export default async (request, response) => {
 	const { webhook } = request.query;
 
@@ -9,12 +15,7 @@ export default async (request, response) => {
 		bot.telegram.setWebhook(webhook);
 		response.send(`webhook set ${webhook}`);
 	} else {
-		const bot = new Telegraf(BOT_TOKEN);
-
-		bot.on('message', async (ctx) => {
-			ctx.reply(ctx.message.text);
-		})
-
+		bot.handleUpdate(request.body);
 	}
 
 	response.send(`telegram bot say Hi.`);
