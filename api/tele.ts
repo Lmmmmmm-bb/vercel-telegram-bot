11180c8e0f45b40ffcf4bcff6aeff4d1c907fa3d
_lmmmmmm <lmmmmmm12138@gmail.com>
2022-11-13 16:37:29 +0800

    chore: catch some error

--- api/tele.ts
+++ api/tele.ts
@@ -3,8 +3,12 @@ import { VercelRequest, VercelResponse } from '@vercel/node';
 import { bot } from '../bot';
 
 export default async (request: VercelRequest, response: VercelResponse) => {
-  console.log('Tele Request Body: ', request.body);
-  await bot.handleUpdate(request.body);
+  try {
+    console.log('Tele Request Body: ', request.body);
+    await bot.handleUpdate(request.body);
+  } catch(err) {
+    return response.status(200).send('Something Error', err, err.message);
+  }
 
   return response.status(200).send('Hi.');
 };
