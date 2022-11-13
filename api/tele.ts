1de59d7bf169ad1129d751ef18de5e4bb0ee983c
_lmmmmmm <lmmmmmm12138@gmail.com>
2022-11-13 17:30:52 +0800

    chore: logger

--- api/tele.ts
+++ api/tele.ts
@@ -3,12 +3,17 @@ import { VercelRequest, VercelResponse } from '@vercel/node';
 import { bot } from '../bot';
 
 export default async (request: VercelRequest, response: VercelResponse) => {
+  console.log('Tele Request Body: ', request.body);
+
   try {
-    console.log('Tele Request Body: ', request.body);
+    console.log('handleUpdate');
     await bot.handleUpdate(request.body);
+    console.log('Success handleUpdate');
   } catch(err) {
-    return response.status(200).send('Something Error', err, err.message);
+    console.log('Something Error', err, err.message);
+    return response.status(200).send(`Something Error ${err.message}`);
   }
+  console.log('Success To Response');
 
   return response.status(200).send('Hi.');
 };
