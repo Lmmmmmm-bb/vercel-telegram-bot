f15ed62a396ab97abd89aabf78ae078ba2ea40f3
_lmmmmmm <lmmmmmm12138@gmail.com>
2022-11-13 12:52:25 +0800

    fix: object log

--- api/tele.ts
+++ api/tele.ts
@@ -3,7 +3,7 @@ import { VercelRequest, VercelResponse } from '@vercel/node';
 import { bot } from '../bot';
 
 export default async (request: VercelRequest, response: VercelResponse) => {
-  console.log(`Tele Request Body: ${request.body}`);
+  console.log('Tele Request Body: ', request.body);
   await bot.handleUpdate(request.body);
 
   return response.send('telegram bot say Hi.');
