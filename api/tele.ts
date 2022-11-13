9498bf8e5c6c3c3f2e61f872edc648c52e37c7a7
_lmmmmmm <lmmmmmm12138@gmail.com>
2022-11-13 16:13:37 +0800

    chore: remove set webhook

--- api/tele.ts
+++ api/tele.ts
@@ -1,10 +1,9 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
 
-import { bot, webhookDomain } from '../bot';
+import { bot } from '../bot';
 
 export default async (request: VercelRequest, response: VercelResponse) => {
   console.log('Tele Request Body: ', request.body);
-  await bot.telegram.setWebhook(webhookDomain);
   await bot.handleUpdate(request.body);
 
   return response.status(200).send('Hi.');
