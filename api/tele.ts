ae639b9849307ece6915f11aaae09de5601601cf
_lmmmmmm <lmmmmmm12138@gmail.com>
2022-11-15 00:20:51 +0800

    chore: remove log

--- api/tele.ts
+++ api/tele.ts
@@ -3,17 +3,12 @@ import { VercelRequest, VercelResponse } from '@vercel/node';
 import { bot } from '../bot';
 
 export default async (request: VercelRequest, response: VercelResponse) => {
-  console.log('Tele Request Body: ', request.body);
-
   try {
-    console.log('handleUpdate');
     await bot.handleUpdate(request.body);
-    console.log('Success handleUpdate');
   } catch(err) {
     console.log('Something Error', err, err.message);
     return response.status(200).send(`Something Error ${err.message}`);
   }
-  console.log('Success To Response');
 
   return response.status(200).send('Hi.');
 };
