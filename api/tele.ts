1d10cc595db8ddab27770fc96eec9d51ea4036d7
_lmmmmmm <lmmmmmm12138@gmail.com>
2023-04-30 00:21:44 +0800

    refactor: lint config

--- api/tele.ts
+++ api/tele.ts
@@ -1,11 +1,12 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
+import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 import { bot } from '../bot';
 
 export default async (request: VercelRequest, response: VercelResponse) => {
   try {
     await bot.handleUpdate(request.body);
-  } catch(err) {
+  } catch (err) {
+    // eslint-disable-next-line no-console
     console.log('Something Error', err, err.message);
     return response.status(200).send(`Something Error ${err.message}`);
   }
--- bot/config.ts
+++ bot/config.ts
@@ -2,5 +2,5 @@ export const botToken = process.env.BOT_TOKEN || '';
 
 export const webhook = {
   domain: process.env.BOT_WEBHOOK_DOMAIN || '',
-  hookPath: process.env.BOT_WEBHOOK_PATH || ''
+  hookPath: process.env.BOT_WEBHOOK_PATH || '',
 };
